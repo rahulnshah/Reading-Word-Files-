33ad5e1bc3c287ab5d9b269680052fc4ef8b335c
Rahul Shah <rahulnshah3012@gmail.com>
2023-05-26 15:35:00 -0400

    added logging statments instead of traditional print statments to enhance UX

--- copy_code_to_text_file_of.py
+++ copy_code_to_text_file_of.py
@@ -1,17 +1,28 @@
 import sys
+import logging
 from docx import Document
 
 def getText(filename):
+    # TODO: add a INFO log statement here
+    logging.info('Trying to read the file content')
     document = Document(filename)
     fullText = ""
     for para in document.paragraphs:
         fullText = fullText + para.text + "\n"
-    return fullText # this is a string like so: "sdfsfsd\nasdsfsf\n
-    
-if __name__ == "__main__":
-    # call getText with filename passed 
-    text_of_docx_file = getText(sys.argv[1]).replace('”','"').replace('“', '"')
+    return fullText
 
-    with open(f"{sys.argv[1][:sys.argv[1].index('.')]}.txt", 'w+') as writer:
-        writer.write(text_of_docx_file)
+if __name__ == "__main__":
+    
+    # initalize  the log setting
+    logging.basicConfig(filename='app.log', level=logging.DEBUG, format="%(asctime)s - %(levelname)s - %(message)s", datefmt="%d-%b-%y %H:%M:%S")
+    try:
+        logging.info('Trying to modify file content to add proper coding syntax')
+        text_of_docx_file = getText(sys.argv[1]).replace('”','"').replace('“', '"')
+
+        with open(f"{sys.argv[1][:sys.argv[1].index('.')]}.txt", 'w+') as writer:
+            writer.write(text_of_docx_file)
+    except IOError as e:
+        logging.error(f"error occured: {str(e)}")
+    except Exception as e:
+        logging.error(f"error occured: {str(e)}")
 
