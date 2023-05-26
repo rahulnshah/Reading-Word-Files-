9599f95dd24c2ce7fbaf86644d0804a34aa9f44d
Rahul Shah <rahulnshah3012@gmail.com>
2023-05-26 16:37:20 -0400

    modified code to process a batch of python files

--- copy_code_to_text_file_of.py
+++ copy_code_to_text_file_of.py
@@ -1,4 +1,5 @@
 import sys
+import os
 import logging
 from docx import Document
 
@@ -15,11 +16,19 @@ if __name__ == "__main__":
     # initalize  the log setting
     logging.basicConfig(filename='app.log', level=logging.DEBUG, format="%(asctime)s - %(levelname)s - %(message)s", datefmt="%d-%b-%y %H:%M:%S")
     try:
-        logging.info('Trying to modify file content to add proper coding syntax')
-        text_of_docx_file = getText(sys.argv[1]).replace('”','"').replace('“', '"')
+        for afile in os.scandir(sys.argv[1]):
+            if afile.is_file() and os.path.splitext(afile)[1] == '.docx':
+                logging.info('Trying to modify file content to add proper coding syntax')
+                text_of_docx_file = getText(afile).replace('”','"').replace('“', '"')
 
-        with open(f"{sys.argv[1][:sys.argv[1].index('.')]}.txt", 'w+') as writer:
-            writer.write(text_of_docx_file)
+                with open(f"{os.path.splitext(afile)[0]}.txt", 'w+') as writer:
+                    writer.write(text_of_docx_file)
+            elif not(afile.is_file()):
+                logging.info(f"{afile} is not a file.")
+            else:
+                logging.info(f"{afile} is not .docx file")
+
+        logging.info("Your docx files have been converted to text files.")
     except IOError as e:
         logging.error(f"error occured: {str(e)}")
     except Exception as e:
